e5b31c4cb727509ac8292d410938376d4e9f1a39
Mateus Nava <nava.mateus@gmail.com>
2017-05-13 18:05:27 -0300

    Remove Active Record

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -4,12 +4,14 @@
 #
 # Read the Guide for Upgrading Ruby on Rails for more info on each option.
 
+Rails.application.config.raise_on_unfiltered_parameters = true
+
 # Make Ruby 2.4 preserve the timezone of the receiver when calling `to_time`.
 # Previous versions had false.
 ActiveSupport.to_time_preserves_timezone = true
 
 # Require `belongs_to` associations by default. Previous versions had false.
-Rails.application.config.active_record.belongs_to_required_by_default = true
+#Rails.application.config.active_record.belongs_to_required_by_default = true
 
 # Do not halt callback chains when a callback returns false. Previous versions had true.
 ActiveSupport.halt_callback_chains_on_return_false = false
